ef4afc23402bdaee02d841531a0461372fad50b7
Nakasukeeeee <>
2022-06-01 21:38:05 +0900

    C#イベントファイルをイベントテキストとレッドエリアにコンポーネントしました。

---
+++ Assets/Scenes/event1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class event1 : MonoBehaviour
+{
+    public Text eventMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventMessage.text="";
+        money=-1000;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
